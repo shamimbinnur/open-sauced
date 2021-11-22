520259e6abc1d4d4ca7b0c7c8a292a551cbf36fd
shamimbinnur <sbnsbn00@gmail.com>
2021-11-22 19:38:42 +0600

    boom

--- pages/components/posts/Posts.jsx
+++ pages/components/posts/Posts.jsx
@@ -7,8 +7,8 @@ function Posts() {
         <div className="bg-darkestGrey py-6 w-full min-h-screen">
             <div className=" container grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6  gap-4 ">
                 {
-                    postData.map(()=>(
-                        <Post/>
+                    postData.map((item, i)=>(
+                        <Post key={i}/>
                     ))
                 }
             </div>
